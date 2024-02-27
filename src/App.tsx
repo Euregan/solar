41119f5e0ed2337c3f48cfa90870a580906f13e6
Valentin Manceaux-Panot <valentin@mcxp.io>
2024-02-27 08:42:31 +0100

    Hid Leva in production

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Canvas } from "@react-three/fiber";
 import { NoToneMapping } from "three";
 import SolarSystem from "./SolarSystem";
+import { Leva } from "leva";
 
 const App = () => (
-  <Canvas
-    gl={{ toneMapping: NoToneMapping }}
-    camera={{ position: [-25, 25, -50] }}
-  >
-    <SolarSystem />
-  </Canvas>
+  <>
+    <Canvas
+      gl={{ toneMapping: NoToneMapping }}
+      camera={{ position: [-25, 25, -50] }}
+    >
+      <SolarSystem />
+    </Canvas>
+    {/* @ts-expect-error TS seems to believe process cannot exist outside of node */}
+    <Leva hidden={process.env.NODE_ENV === "production"} />
+  </>
 );
 
 export default App;
